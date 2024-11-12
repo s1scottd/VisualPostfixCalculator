b3ef22259f3a3432789e8574caa8db226678e0e8
Scott Davis <scottdavis.gv@gmail.com>
2024-11-11 16:01:04 -0800

    Added a manifest file and module-info.java file

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module VisualPostfixCalculator {
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires org.kordamp.ikonli.javafx;
+  requires org.kordamp.ikonli.fontawesome;
+  requires org.kordamp.ikonli.fontawesome5;
+
+  exports com.s1scottd; // Replace with your package
+}
